cabc38d8ce79970285fa351326f5d49c80b3d6e0
lfeitosaf <lucas-jpl@hotmail.com>
2023-05-05 13:41:03 -0300

    fix: fixed migrations

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -21,7 +21,7 @@ const listUsersController = async (req: Request, res: Response) => {
 };
 
 const deleteUserController = async (req: Request, res: Response) => {
-  await deleteUserService(req.params.id);
+  await deleteUserService(req.user.id);
 
   return res.status(204).send();
 };
--- src/imports/index.ts
+++ src/imports/index.ts
@@ -1,6 +1,7 @@
 // import { Entity2 } from './path/to/entity2';
 import { Contact, User } from "../entities";
+import { createTable1683304740900 } from "../migrations/1683304740900-createTable";
 
 export const entities = [User, Contact];
 
-export const migrations = [];
+export const migrations = [createTable1683304740900];
